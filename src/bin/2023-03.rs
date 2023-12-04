6b233b4dea24b7efe2197b8ed6005cc7f1193740
Kerry Wang <kerrywang369@gmail.com>
2023-12-03 22:27:12 -0700

    day 3 done

--- src/bin/2023-03.rs
+++ src/bin/2023-03.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use adventofcode::*;
 use regex::Regex;
@@ -24,6 +24,71 @@ fn neighbours(
     let ys = std::cmp::max(1, y) - 1..=std::cmp::min(y + 1, y_max - 1);
     xs.flat_map(move |x_| ys.clone().map(move |y_| (x_, y_)))
         .filter(move |&(x_, y_)| x_ != x || y_ != y)
+}
+
+fn part2() {
+    let input = get_input();
+
+    // Populate numbers
+    let mut numbers = vec![];
+    let mut coord_to_index = HashMap::new();
+    for (i, line) in input.lines().enumerate() {
+        let re = Regex::new(r"([0-9]+)").unwrap();
+        for capture in re.captures_iter(line) {
+            let (_, [value]) = capture.extract();
+            let start = capture.get(1).unwrap().start();
+            let end = capture.get(1).unwrap().end();
+            let value = value.parse::<usize>().unwrap();
+            numbers.push(value);
+            for idx in start..end {
+                coord_to_index.insert(Coord { 0: i, 1: idx }, numbers.len() - 1);
+            }
+        }
+    }
+
+    // Populate symbols
+    let mut symbols = HashMap::new();
+    for (i, line) in input.lines().enumerate() {
+        let re = Regex::new(r"([^[0-9].])").unwrap();
+        for capture in re.captures_iter(line) {
+            let (_, [value]) = capture.extract();
+            let start = capture.get(1).unwrap().start();
+            symbols.insert(Coord { 0: i, 1: start }, value.to_owned());
+        }
+    }
+
+    let (line_width, line_height) = {
+        let mut line_width = 0;
+        let mut line_height = 0;
+        for line in input.lines() {
+            line_width = line_width.max(line.len());
+            line_height += 1;
+        }
+        (line_width, line_height)
+    };
+
+    // Find symbols with exactly two number neighbours
+    // Sum up the product of the numbers
+    let mut sum_of_gears = 0;
+    for symbol in symbols.keys() {
+        let neighbours = neighbours(symbol.0, symbol.1, line_width, line_height);
+
+        let mut neighbour_indices = HashSet::new();
+        for neighbour in neighbours {
+            if let Some(idx) = coord_to_index.get(&Coord {
+                0: neighbour.0,
+                1: neighbour.1,
+            }) {
+                neighbour_indices.insert(*idx);
+            }
+        }
+
+        if let [idx1, idx2] = neighbour_indices.iter().collect::<Vec<_>>()[..] {
+            sum_of_gears += numbers[*idx1] * numbers[*idx2];
+        }
+    }
+
+    dbg!(&sum_of_gears);
 }
 
 fn part1() {
@@ -95,5 +160,5 @@ fn part1() {
 }
 
 fn main() {
-    part1();
+    part2();
 }
